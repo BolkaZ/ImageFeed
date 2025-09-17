1e2d58f508833db1c0700e2f640435ac63ce04a9
Bolkaz <kchrg23@gmail.com>
2025-09-17 12:38:10 +0300

    make outlet and action for profile screen

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -3,4 +3,12 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
+    @IBOutlet var profileImageView: UIImageView!
+    @IBOutlet var nameLable: UILabel!
+    @IBOutlet var userNameLabel: UILabel!
+    @IBOutlet var profileDescriptionLabel: UILabel!
+    @IBOutlet var logoutButton: UIButton!
+    
+    @IBAction func logoutTapped() {
+    }
 }
