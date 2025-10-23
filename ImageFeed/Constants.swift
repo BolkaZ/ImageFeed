9207ac078ee88798c6d4fa1e7156679e381d1cc2
Bolkaz <kchrg23@gmail.com>
2025-10-23 18:37:18 +0300

    made enum Constants

--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -1,8 +1,9 @@
-//
-//  Constants.swift
-//  ImageFeed
-//
-//  Created by bolkaz on 23.10.2025.
-//
-
 import Foundation
+
+enum Constants {
+    static let accessKey = "<ваш Access Key>"
+    static let secretKey = "<ваш Secret Key>"
+    static let redirectURI = "<ваш Redirect URI>"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}a
